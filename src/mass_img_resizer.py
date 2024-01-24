5f2c5d7527deffee99b44d2cb6fec4ad248b15b5
Ellis Hogan <ellishogan@Elliss-MacBook-Air.local>
2024-01-23 22:19:00 -0500

    update

--- src/mass_img_resizer.py
+++ src/mass_img_resizer.py
@@ -5,3 +5,6 @@ def mass_resize(images_data):
 		
 		height, width = data['size']
 		resize(data['src'], f"{data['src']}_resize_{height}_{width}", (height, width))
+  
+if __name__ == "__main__":
+  mass_resize([{'src': 'path_to_input_dir', 'size': (800, 600)}])
